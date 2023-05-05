5bdd86723d32e877ba16a0451dc7c40db6049e09
lms8262 <130732104+lms8262@users.noreply.github.com>
2023-05-06 03:54:00 +0900

    [unrated] Title: 원하는 문자열 찾기, Time: 523.17 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -1,8 +1,8 @@
 class Solution {
     public int solution(String myString, String pat) {
         int answer = 0;
-        myString = myString.toUpperCase();
-        pat = pat.toUpperCase();
+        myString = myString.toLowerCase();
+        pat = pat.toLowerCase();
         for(int i=0; i<myString.length(); i++) {
             if(myString.indexOf(pat,i) != -1) {
                 answer = 1;
