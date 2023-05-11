ed7fb83aad390e7030800abf372a1e879477534e
lms8262 <130732104+lms8262@users.noreply.github.com>
2023-05-12 00:13:55 +0900

    [unrated] Title: A 강조하기, Time: 0.04 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
@@ -1,13 +1,7 @@
 class Solution {
     public String solution(String myString) {
-        StringBuffer answer = new StringBuffer();
-        for(int i=0; i<myString.length(); i++) {
-            if(myString.substring(i,i+1).equals("a") || myString.substring(i,i+1).equals("A")) {
-                answer.append(myString.substring(i,i+1).toUpperCase());
-            } else {
-                answer.append(myString.substring(i,i+1).toLowerCase());
-            }
-        }
-        return answer.toString();
+        myString = myString.toLowerCase();
+        myString = myString.replace('a','A');
+        return myString;
     }
 }
