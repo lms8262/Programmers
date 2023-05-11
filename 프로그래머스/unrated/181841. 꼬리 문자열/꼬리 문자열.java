5cd5cf1fc4f033dfb7a597c335447779fd4e76b9
lms8262 <130732104+lms8262@users.noreply.github.com>
2023-05-11 23:37:31 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.13 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.java
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.java
@@ -1,11 +1,11 @@
 class Solution {
     public String solution(String[] str_list, String ex) {
-        String answer = "";
+        StringBuffer answer = new StringBuffer();
         for(int i=0; i<str_list.length; i++) {
             if(str_list[i].indexOf(ex) == -1) {
-                answer += str_list[i];
+                answer.append(str_list[i]);
             }
         }
-        return answer;
+        return answer.toString();
     }
 }
