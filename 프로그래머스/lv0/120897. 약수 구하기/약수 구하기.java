56b6f8905922981dad044c122f5728a04a421f60
lms8262 <130732104+lms8262@users.noreply.github.com>
2023-05-16 11:28:58 +0900

    [level 0] Title: 약수 구하기, Time: 0.02 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int n) {
+        int size = 0;
+        for(int i=1; i<=n; i++) {
+            if(n % i == 0) {
+                size++;
+            }
+        }
+        int[] answer = new int[size];
+        int count = 0;
+        for(int i=1; i<=n; i++) {
+            if(n % i == 0) {
+                answer[count] = i;
+                count++;
+            }
+        }
+        return answer;
+    }
+}
