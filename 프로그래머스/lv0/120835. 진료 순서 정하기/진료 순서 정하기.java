b7d10e5a905459ad74277c7481f536a482d0143b
lms8262 <130732104+lms8262@users.noreply.github.com>
2023-06-14 15:39:11 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.41 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] emergency) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int[] answer = new int[emergency.length];
+        int[] sortEmergency = Arrays.copyOf(emergency, emergency.length); // 배열복사
+        Arrays.sort(sortEmergency); // 오름차순 정렬
+        for(int i=0; i<sortEmergency.length; i++) {
+            map.put(sortEmergency[i],sortEmergency.length-i); // key에 기존 배열 값, value에 진료순서
+        }
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = map.get(emergency[i]); // 기존 배열에 대응한 진료순서
+        }
+        return answer;
+    }
+}
