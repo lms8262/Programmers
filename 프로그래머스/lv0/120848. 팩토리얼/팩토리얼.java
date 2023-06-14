927a909cd87918615ac6cd5489650f14982dd444
lms8262 <130732104+lms8262@users.noreply.github.com>
2023-06-14 12:59:39 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
@@ -3,12 +3,11 @@ class Solution {
         int answer = 0;
         int mul = 1;
         for(int i=1; i<=n; i++) {
+            answer = i;
             mul *= i;
             if(mul > n) {
                 answer = i-1;
                 break;
-            } else {
-                answer = i;
             }
         }
         return answer;
