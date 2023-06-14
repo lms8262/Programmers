bcf8725ea779b7172cc90c2d48c36019c2b6756c
lms8262 <130732104+lms8262@users.noreply.github.com>
2023-06-14 18:20:29 +0900

    [unrated] Title: 세로 읽기, Time: 0.03 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, int m, int c) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0; i<my_string.length()/m; i++) {
+            sb.append(my_string.substring(c+m*i-1,c+m*i));
+        }
+        return sb.toString();
+    }
+}
