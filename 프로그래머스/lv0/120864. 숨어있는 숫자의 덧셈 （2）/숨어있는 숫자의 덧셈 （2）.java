16b2e118e54c411fba33b344e334cf41f4e47b6f
lms8262 <130732104+lms8262@users.noreply.github.com>
2023-06-14 17:21:51 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.15 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -1,16 +1,16 @@
+import java.util.*;
+
 class Solution {
     public int solution(String my_string) {
         int answer = 0;
-        if(!my_string.matches(".*[0-9].*")) {
-            return answer;
-        }
+        
         my_string = my_string.replaceAll("[^0-9]", " ");
-        my_string = my_string.trim();
-        my_string = my_string.replaceAll(" +", " ");
-        String[] strArray = my_string.split("\\s");
-        for(String s : strArray) {
-            answer += Integer.parseInt(s);
+        
+        StringTokenizer st = new StringTokenizer(my_string);
+        while(st.hasMoreTokens()) {
+            answer += Integer.parseInt(st.nextToken());
         }
+        
         return answer;
     }
 }
