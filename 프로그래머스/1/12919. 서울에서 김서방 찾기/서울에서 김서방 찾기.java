19600ea4ea9ab5b29f4139dce60e3c0d0ea49d90
lms8262 <130732104+lms8262@users.noreply.github.com>
2024-02-05 00:28:34 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.10 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,11 +1,14 @@
+import java.lang.*;
+
 class Solution {
     public String solution(String[] seoul) {
-        String answer = "김서방은 ";
+        StringBuilder sb = new StringBuilder();
+        sb.append("김서방은 ");
         for(int i=0; i<seoul.length; i++) {
             if(seoul[i].equals("Kim")) {
-                answer += i+"에 있다";
+               sb.append(i).append("에 있다");
             }
         }
-        return answer;
+        return sb.toString();
     }
 }
