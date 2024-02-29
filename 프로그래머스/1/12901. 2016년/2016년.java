cdbe20f5177186e3b7a8ae96a5f6eb02e90cf631
lms8262 <130732104+lms8262@users.noreply.github.com>
2024-03-01 00:47:58 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,12 +1,11 @@
 class Solution {
     public String solution(int a, int b) {
-        int[][] md = {{1,31},{2,29},{3,31},{4,30},{5,31},{6,30},
-                           {7,31},{8,31},{9,30},{10,31},{11,30},{12,31}};
+        int[] day = {31,29,31,30,31,30,31,31,30,31,30,31};
         String[] week = {"THU","FRI","SAT","SUN","MON","TUE","WED"};
         int day365 = 0;
         
         for(int i=0; i<a-1; i++) {
-            day365 += md[i][1];
+            day365 += day[i];
         }
         day365 += b;
         
