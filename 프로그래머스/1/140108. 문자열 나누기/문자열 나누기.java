d0bc2ff4a4b532aa5c09be381c46e99ddec4b895
lms8262 <130732104+lms8262@users.noreply.github.com>
2024-03-08 23:54:55 +0900

    [level 1] Title: 문자열 나누기, Time: ., Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        int count1 = 0;
+        int count2 = 0;
+        String x = "";
+        for(int i=0; i<s.length(); i++) {
+            if(count1 == 0 && count2 == 0) {
+                x = s.substring(i,i+1);    
+            }
+            
+            if(s.substring(i,i+1).equals(x)) {
+                count1++;
+            } else {
+                count2++;
+            }
+            
+            if(count1 == count2) {
+                answer++;
+                count1 = 0;
+                count2 = 0;
+            }
+        }
+        
+        if(count1 != count2) {
+            answer++;
+        }
+        
+        return answer;
+    }
+}
